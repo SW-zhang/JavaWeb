676e25e5e096595842bda9a9d10ea6415d81fee2
SW-zhang <zhangshuwang1990@163.com>
2017-03-09 17:03:16 +0800

    添加注释

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,16 +5,12 @@ import com.services.demo.service.DemoService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
-import java.util.Date;
-
 public class Test {
 
     public static void main(String[] args) {
         ApplicationContext context = new FileSystemXmlApplicationContext("classpath:/applicationContext.xml");
         DemoService action = (DemoService) context.getBean("demoService");
-        Demo d = new Demo();
-        d.setCreateTime(new Date());
-        action.add(d);
+        action.add(new Demo());
     }
 
 }
